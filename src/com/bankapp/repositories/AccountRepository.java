14f39094783b008b04d329e8cee702ac59aa4cf5
ShubhaAzhakiyanambi <78926976+ShubhaAzhakiyanambi@users.noreply.github.com>
2021-02-13 23:22:55 +0530

    AccountRepository file added

---
+++ src/com/bankapp/repositories/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.bankapp.repositories;
+
+import com.bankapp.models.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByAccountNumber(String accountNumber);
+}
