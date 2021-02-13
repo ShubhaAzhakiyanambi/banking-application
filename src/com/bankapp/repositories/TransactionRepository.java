a5ac1310a8128b1bac4247869df66ed35f79efaf
ShubhaAzhakiyanambi <78926976+ShubhaAzhakiyanambi@users.noreply.github.com>
2021-02-13 23:23:43 +0530

    Transaction repository file added

---
+++ src/com/bankapp/repositories/TransactionRepository.java
@@ -0,0 +1,11 @@
+package com.bankapp.repositories;
+
+import com.bankapp.models.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findBySourceAccountIdOrderByInitiationDate(long id);
+}
