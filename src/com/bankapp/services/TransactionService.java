3c7778fc3ef6afda0c5c9501c73c777e91336ef6
ShubhaAzhakiyanambi <78926976+ShubhaAzhakiyanambi@users.noreply.github.com>
2021-02-13 23:28:22 +0530

    Service class added for transactions

---
+++ src/com/bankapp/services/TransactionService.java
@@ -0,0 +1,65 @@
+package com.bankapp.services;
+
+import com.bankapp.models.Account;
+import com.bankapp.models.Transaction;
+import com.bankapp.repositories.AccountRepository;
+import com.bankapp.repositories.TransactionRepository;
+import com.bankapp.utils.TransactionInput;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class TransactionService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    public boolean makeTransfer(TransactionInput transactionInput) {
+        LOGGER.debug("Triggered TransactionService.transactionInput");
+        String sourceAccountNumber = transactionInput.getSourceAccount().getAccountNumber();
+        Optional<Account> sourceAccount = accountRepository
+                .findByAccountNumber(sourceAccountNumber);
+
+        String targetAccountNumber = transactionInput.getTargetAccount().getAccountNumber();
+        Optional<Account> targetAccount = accountRepository
+                .findByAccountNumber(targetAccountNumber);
+
+        if (sourceAccount.isPresent() && targetAccount.isPresent()) {
+            if (isAmountAvailable(transactionInput.getAmount(), sourceAccount.get().getCurrentBalance())) {
+                var transaction = new Transaction();
+
+                transaction.setAmount(transactionInput.getAmount());
+                transaction.setSourceAccountId(sourceAccount.get().getId());
+                transaction.setTargetAccountId(targetAccount.get().getId());
+                transaction.setTargetOwnerName(targetAccount.get().getOwnerName());
+                transaction.setInitiationDate(LocalDateTime.now());
+                transaction.setCompletionDate(LocalDateTime.now());
+                transaction.setReason(transactionInput.getReason());
+
+                updateAccountBalance(sourceAccount.get(), transactionInput.getAmount());
+                transactionRepository.save(transaction);
+				LOGGER.debug("Amount transferred successfully");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void updateAccountBalance(Account account, double amount) {
+		LOGGER.debug("Entering updateAccountBalance method");
+        account.setCurrentBalance((account.getCurrentBalance() - amount));
+        accountRepository.save(account);
+		LOGGER.debug("Savings account balance updated");
+    }
+
+    // TODO support overdrafts or credit account
+    private boolean isAmountAvailable(double amount, double accountBalance) {
+        return (accountBalance - amount) > 0;
+    }
+}
